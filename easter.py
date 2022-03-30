352e09778715435de8a8824e7358c3ed4c7da87e
2021_ <72861096+i2021@users.noreply.github.com>
2022-03-30 11:30:06 +0200

    complete redesign w enterprise lvl code

--- easter.py
+++ easter.py
@@ -1,15 +1,30 @@
+import datetime
+
 from matplotlib import pyplot as plt
 
+print("(1) --> Calculate easter in year range")
+print("(2) --> Calculate easter for specific year")
+
 while True:
-    low = input("Year low bound: (default --> 1000) ") or 1000
-    high = input("Year high bound: (default --> 3000) ") or 3000
+    option = input("Choose (1) / (2) ")
     try:
-        low = int(low)
-        high = int(high)
-        print("Calculating easter in range:", low, "-->", high)
+        option = int(option)
+        if option == 1:
+            break
+        else:
+            while True:
+                year = datetime.datetime.now().year
+                year = input("Specify year ") or year
+                try:
+                    y = int(year)
+                    print("Using year", year)
+                    break
+                except ValueError:
+                    print("This is not a valid year. Please enter a valid year")
         break
     except ValueError:
         print("This is not a valid year. Please enter a valid year")
+
 
 # g - Golden year - 1
 # c - Century
@@ -17,25 +32,41 @@ while True:
 # i - Number of days from March 21 to Paschal Full Moon (PFM)
 # j - Weekday for PFM (0=Sunday, etc)
 # p - Number of days from March 21 to Sunday on or before PFM (-6 to 28)
-for y in range(low, high, 1):
+def p_grabber():
     g = y % 19
     c = y // 100
     h = (c - c // 4 - (8 * c + 13) // 25 + 19 * g + 15) % 30
     i = h - (h // 28) * (1 - (h // 28) * (29 // (h + 1)) * ((21 - g) // 11))
     j = (y + y // 4 + i + 2 - c + c // 4) % 7
 
-    # p can be from -6 to 28
-    p = i - j
+    # return can be from -6 to 28
+    return i - j
+
+
+if option == 1:
+    while True:
+        low = input("Year low bound: (default --> 1000) ") or 1000
+        high = input("Year high bound: (default --> 3000) ") or 3000
+        try:
+            low = int(low)
+            high = int(high)
+            print("Calculating easter in range:", low, "-->", high)
+            break
+        except ValueError:
+            print("This is not a valid year. Please enter a valid year")
+
+    for y in range(low, high, 1):
+        # plotting the points on the graph
+        # x --> date (p)
+        # y --> year (y)
+        plt.plot(p_grabber(), y, marker="o", markersize=2, markeredgecolor="red", markerfacecolor="red")
+    # show the graph :D
+    plt.xlabel("The date number")
+    plt.ylabel("Year")
+    plt.title("Easter day in  a given year")
+    plt.show()
+else:
+    p = p_grabber()
     d = 1 + (p + 27 + (p + 6) // 40) % 31
     m = 3 + (p + 26) // 30
-    # plotting the points on the graph
-    # x --> date (p)
-    # y --> year (y)
-    plt.plot(p, y, marker="o", markersize=2, markeredgecolor="red",
-             markerfacecolor="red")  # print(datetime.date(int(y), int(m), int(d)))
-
-# show the graph :D
-plt.xlabel("The date number")
-plt.ylabel("Year")
-plt.title("Easter day in  a given year")
-plt.show()
+    print(datetime.date(int(year), int(m), int(d)))
